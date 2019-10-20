5eda8c0f8133450cea177441f44d19191b3c2233
Cristina <ckuo@fi.uba.ar>
2019-10-20 19:41:48 -0300

    Minor refactor

--- SimplePerceptron.py
+++ SimplePerceptron.py
@@ -26,21 +26,12 @@ class SimplePerceptron():
         self.weight_matrix = np.zeros(self.input_dimension)
         self.num_patterns = cols
 
-#    def init(self, x, y):
-#        self.input_dim,_ = x.shape
-#        self.num_patterns = y.size
-#        self.x = x
-#        self.y_desired = y
-#        
-#        self.W = np.zeros(self.input_dim)
-#        self.etha = etha # Learning rate
-
     def train(self):
         iter_counter = 0
         train_success = False
 
         # Initialize weight vector in random numbers
-        self.W = np.random.rand(self.input_dimension)
+        self.weight_matrix = np.random.rand(self.input_dimension)
         
         # Train until trained output is equal to desired output or until maximum number of iterations is reached
         while True:
@@ -55,10 +46,10 @@ class SimplePerceptron():
 
                 delta_W = self.learning_rate * input_pattern * (output_pattern-actual_output)
                 
-                self.W = self.W + delta_W
+                self.weight_matrix += delta_W
 
             expectation = self.patterns_output    
-            reality = self.get_output(self.patterns_input)
+            reality = self.evaluate(self.patterns_input)
 
             if np.array_equal(expectation,reality):
                 train_success = True
@@ -69,17 +60,17 @@ class SimplePerceptron():
         return train_success    
     
     
-    def get_output(self, x_input): # Expects a list or an array
+    def evaluate(self, x_input): # Expects a list or an array
         # Convert to numpy array
         x_input = np.array(x_input)
 
         # Validate input dimension is correct
         input_rows = x_input.shape[0] 
-        if input_rows != len(self.W):
-            log.error("Input dimension {} doesnt match weight matrix dimension {}.".format(x_input.shape,self.W.shape))
+        if input_rows != len(self.weight_matrix):
+            log.error("Input dimension {} doesnt match weight matrix dimension {}.".format(x_input.shape,self.weight_matrix.shape))
             sys.exit(1)
         
-        result = np.dot(self.W, x_input)
+        result = np.dot(self.weight_matrix, x_input)
         result = np.vectorize(sgn)(result)
         return result           
 
@@ -98,6 +89,6 @@ if __name__ == '__main__':
         print("TRAIN SUCCESS")
     else:
         print("TRAIN FAILURE")
-    log.info("Output obteined from trained perceptron: {}".format( perceptron.get_output(x) ) )
+    log.info("Output obteined from trained perceptron: {}".format( perceptron.evaluate(x) ) )
 
 
